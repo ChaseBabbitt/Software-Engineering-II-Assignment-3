17d5cc075cc1c21374faf2fc3d10603ef7af4110
ChaseBabbitt <chasebabbitt@yahoo.com>
2017-02-19 20:14:49 -0800

    Added new packages for cards for our second game "Hearthclone". And some minor bugfixes for the DefenseStrategy for the Hex game.

---
+++ CardGame/src/com/chasebabbitt/cardgame/CardGame.java
@@ -0,0 +1,78 @@
+package com.chasebabbitt.cardgame;
+
+
+
+import com.chasebabbitt.cardgame.player.Player;
+
+
+public class CardGame {
+	
+
+	//Duel duel;
+	public static void main(String[] args) {
+		//Scanner input = new Scanner(System.in);
+		HexDuel duel;
+		Player player = new Player("Player 1",20);
+		Player other = new Player("Player 2",20);
+		Player winner;
+		//Player attacker, defender, temp;
+		
+		player.setRandomCaseD();
+		player.setOpponent(other);
+		player.setSmartDefenseStrategy();
+		other.setRandomCaseA();
+		other.setOpponent(player);
+		
+		duel = new HexDuel(player,other);
+		winner = duel.play();
+		System.out.println(winner.getName()+" wins.");
+		/*
+		attacker = other;
+		defender = player;
+		
+		while(player.getHealth()>0 && other.getHealth()>0){
+			other.toConsole();
+			player.toConsole();
+			
+			attacker.setAttackingCards();
+			defender.setDefendingCards();
+		
+			System.out.println(attacker.getName()+" is now attacking.");
+			
+			Move move = defender.getMove();
+			while(move!=null){
+				//move.consoleDisplay();
+				move.execute();
+				move = defender.getMove();
+			}
+		
+		
+			input.nextLine();
+		
+			// switch attacker and defender
+			temp = attacker;
+			attacker = defender;
+			defender = temp;
+		}
+		
+		other.toConsole();
+		player.toConsole();
+		
+		/*System.out.println(attacker.getName()+" is now attacking.");
+		
+		other.toConsole();
+		player.toConsole();
+		
+		attacker.setAttackingCards();
+		defender.setDefendingCards();
+		move = defender.getMove();
+		while(move!=null){
+			move.consoleDisplay();
+			move.execute();
+			move = defender.getMove();
+		}
+		input.close();*/
+
+	}
+
+}
---
+++ CardGame/src/com/chasebabbitt/cardgame/cards/hearthclone/HearthcloneAbilitiesDecorator.java
@@ -0,0 +1,22 @@
+package com.chasebabbitt.cardgame.cards.hearthclone;
+
+import com.chasebabbitt.cardgame.cards.AbilitiesDecorator;
+import com.chasebabbitt.cardgame.cards.Card;
+
+public class HearthcloneAbilitiesDecorator extends AbilitiesDecorator{
+	
+	// Bit masks for Hearthclone keywords
+	public static final int TAUNT = 1;
+	public static final int DIVINESHIELD = 2;
+	public static final int ENRAGE = 4;
+	public static final int BATTLECRY = 8;
+	public static final int CHARGE = 16;
+	public static final int DEATHRATTLE = 32;
+	public static final int STEALTH = 64;
+	public static final int WINDFURY = 128;
+	
+	public HearthcloneAbilitiesDecorator(Card card) {
+		super(card);
+	}
+
+}
