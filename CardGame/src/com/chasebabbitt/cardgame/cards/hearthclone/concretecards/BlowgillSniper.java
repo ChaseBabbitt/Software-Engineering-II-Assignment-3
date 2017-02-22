b6141a51510dc08e0192053cff94068cb3555994
Riflewolf <Robert.jkimbrough@gmail.com>
2017-02-22 14:59:25 -0800

    Added 3 new murloc cards and a Charge decloration

---
+++ CardGame/src/com/chasebabbitt/cardgame/cards/hearthclone/concretecards/BlowgillSniper.java
@@ -0,0 +1,15 @@
+package com.chasebabbitt.cardgame.cards.hearthclone.concretecards;
+
+import com.chasebabbitt.cardgame.cards.Card;
+
+public class BlowgillSniper extends Card{
+	public BlowgillSniper(){
+		name = "Blowgill Sniper";
+		attackpoints = 2;
+		defensepoints = 1;
+		cost = 2;
+		keywords = 0;
+		//deal 1 damage to target
+	}
+
+}
---
+++ CardGame/src/com/chasebabbitt/cardgame/cards/hearthclone/concretecards/MurlocRaider.java
@@ -0,0 +1,13 @@
+package com.chasebabbitt.cardgame.cards.hearthclone.concretecards;
+
+import com.chasebabbitt.cardgame.cards.Card;
+
+public class MurlocRaider extends Card {
+	public MurlocRaider(){
+		name = "Murloc Raider";
+		attackpoints = 2;
+		defensepoints = 1;
+		cost = 1;
+		keywords = 0; // none
+	}
+}
