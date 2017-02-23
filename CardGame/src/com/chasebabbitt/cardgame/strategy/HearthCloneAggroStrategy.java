67f9badef8a58d148fa30ec332b1eca0f38f7e8e
Riflewolf <Robert.jkimbrough@gmail.com>
2017-02-23 03:15:36 -0800

    Reworked both strategies to deal with all null pointers

--- CardGame/src/com/chasebabbitt/cardgame/strategy/HearthCloneAggroStrategy.java
+++ CardGame/src/com/chasebabbitt/cardgame/strategy/HearthCloneAggroStrategy.java
@@ -8,45 +8,42 @@ import com.chasebabbitt.cardgame.player.Player;
 public class HearthCloneAggroStrategy implements Strategy {
 
 	@Override
-	public Move getMove(Player Defender, Player Attacker){
+	public Move getMove(Player Attacker, Player Defender){
+		
 		Move move = null;
-		Card attack = null;
+		
 		//Plays the strongest attacker card it can
 		if(CanPlayCard(Attacker)){
-			ArrayList<Card> possiblecards = PlayableCard(Attacker);
-			attack = possiblecards.get(0);
-			for(Card c : possiblecards){
-				if(c.getAttackPoints()<attack.getAttackPoints())
-						attack = c;
-			}
-			move = new PlayCard(attack,Attacker);
+			move = PlayCard(Attacker);
 			return move;
 		}
 		
-		attack = null;
 		//Get All cards that can be attacked
 		ArrayList<Card> ReadyCards = ReadyCard(Attacker);
 		
+		//If there are ready cards make an attack
 		if(ReadyCards.size()>0){
+			
 			ArrayList<Card> LegalTargets = new ArrayList<Card>();
 			
-			for(Card c: Defender.getCards()){
-				if(c.getKeywords()==1){
-					LegalTargets.add(c);
+			//If the enemy has cards, check for taunt
+			if(hasDefender(Defender)){
+				LegalTargets = findTaunt(Defender);
+				//if there is taunt, find the best attack
+				if(LegalTargets.size()>0){
+					move = FindTarget(ReadyCards,LegalTargets,Attacker,Defender);
+					return move;
+				}
+				else{
+					ReadyCards.get(0).exhaust();
+					move = new UnblockedAttack(ReadyCards.get(0), Attacker, Defender);
+					return move;
 				}
 			}
-			if(LegalTargets.size()>0){
-				attack = ReadyCards.get(0);
-				for(Card k : ReadyCards){
-					if(attack.getAttackPoints()>k.getAttackPoints()){
-						attack = k;
-					}
-				}
-				move = new BlockedAttack(attack,LegalTargets.get(0),Attacker,Defender);
-				return move;
-			}
+			
 			//if there are no cards with taunt attack the player
-			if(LegalTargets.size()==0){
+			
+			else{
 				ReadyCards.get(0).exhaust();
 				move = new UnblockedAttack(ReadyCards.get(0), Attacker, Defender);
 				return move;
@@ -54,19 +51,120 @@ public class HearthCloneAggroStrategy implements Strategy {
 		}
 		return move;
 	}
+	
+	
+	
 	/**
+	 * CanPlayCard
 	 * Checks the players hand to see if he can play one or more cards.
 	 * @param Attacker
 	 * @return true if there is at least 1 card the player can play to the field
 	 */
 	private Boolean CanPlayCard(Player Attacker){
-		for(Card c: Attacker.getHand()){
+		ArrayList<Card> Temp = Attacker.getHand();
+		if(Temp.size() == 0)
+		{ return false;}
+		for(Card c: Temp){
 			if(c.getCost()<Attacker.getResources())
 				return true;
 		}
 		return false;
+
 	}
+	
+	
+	
 	/**
+	 * PlayCard 
+	 * Finds the strongest card to play
+	 * @param Attacker
+	 * @return a move of a card to be played.
+	 */
+	private Move PlayCard(Player Attacker){
+		Move move = null;
+		
+		ArrayList<Card> possiblecards = PlayableCard(Attacker);
+		Card possible = possiblecards.get(0);
+		for(Card c : possiblecards){
+			if(c.getAttackPoints()>possible.getAttackPoints())
+					possible = c;
+		}
+		move = new PlayCard(possible,Attacker);
+		return move;
+	}
+	
+	/**
+	 * hasDefender
+	 * @param defender
+	 * @return true if the oppenent has 1 or more cards on the field
+	 */
+	private Boolean hasDefender(Player defender){
+		System.out.println("hasDefender");
+		if(defender.getCards().size()>0)
+			return true;
+		else
+			return false;
+	}
+	
+	/**
+	 * findTaunt
+	 * @param defender
+	 * @return a list of cards the oppenent controls with taunt
+	 */
+	private ArrayList<Card> findTaunt(Player defender){
+		ArrayList<Card> LegalTargets = new ArrayList<Card>(); 
+		for(Card c: defender.getCards()){
+			if(c.getKeywords()==1){
+				LegalTargets.add(c);
+			}
+		}
+		return LegalTargets;
+	}
+	
+	
+	/**
+	 * FindTarget
+	 * @param Ready
+	 * @param Target
+	 * @param one
+	 * @param two
+	 * @return a BlockedAttackMove
+	 */
+	private Move FindTarget(ArrayList<Card> Ready, ArrayList<Card> Target, Player one, Player two){
+		Card Attacker = Ready.get(0);
+		Card Defender;
+		Move move = null;
+		for(Card c : Target){
+			for(Card k: Ready){
+				if(k.getAttackPoints()>c.getDefensePoints()&&Attacker.getAttackPoints()>k.getAttackPoints()){
+					Attacker = k;
+				}
+			}
+			if(Attacker.getAttackPoints()>c.getDefensePoints()){
+				move = new BlockedAttack(Attacker,c,one,two);
+				return move;
+			}
+		}
+		Attacker = Ready.get(0);
+		for(Card k : Ready){
+			if(Attacker.getAttackPoints()>k.getAttackPoints()){
+				Attacker = k;
+			}
+		}
+		Defender = Target.get(0);
+		for(Card c: Target){
+			if(Defender.getDefensePoints()>c.getDefensePoints()){
+				Defender = c;
+			}
+		}
+		move = new BlockedAttack(Attacker,Defender,one,two);
+		return move;
+		
+	}
+	
+
+	/**
+	 * PlayableCard
 	 * Gets a list of all the cards the player could play
 	 * @param Attacker
 	 * @return a list of all cards that cost less than the players current resources
@@ -80,7 +178,13 @@ public class HearthCloneAggroStrategy implements Strategy {
 		}
 		return playable;
 	}
+	
+	
+	
+	
+	
 	/**
+	 * ReadyCard
 	 * Gets all untapped cards
 	 * @param Attacker
 	 * @return a list of ready cards on the players field
