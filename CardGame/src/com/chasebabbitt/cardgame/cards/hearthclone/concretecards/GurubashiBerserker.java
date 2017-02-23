6355f5049ef9d131db7535a5cbf1473a1ad9412e
Chase Babbitt <chasebabbitt@yahoo.com>
2017-02-22 23:05:50 -0800

    Created a GurubashiBerserker Card and a Junit test case for it

---
+++ CardGame/src/com/chasebabbitt/cardgame/cards/hearthclone/concretecards/GurubashiBerserker.java
@@ -0,0 +1,15 @@
+package com.chasebabbitt.cardgame.cards.hearthclone.concretecards;
+
+import com.chasebabbitt.cardgame.cards.Card;
+
+public class GurubashiBerserker extends Card{
+	public GurubashiBerserker(){
+		name = "Gurubashi Berserker";
+		attackpoints = 2;
+		defensepoints = 7;
+		cost = 5;
+		keywords = 0;
+		tapped = true;
+	}
+
+}
